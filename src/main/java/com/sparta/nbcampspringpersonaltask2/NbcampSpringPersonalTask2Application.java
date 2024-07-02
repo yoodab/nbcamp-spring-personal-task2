95b2c77138cdf10d0d4fdaf1fced647be8ab6cb4
yoodab <yoondab2166@gmail.com>
2024-07-02 20:47:53 +0900

    init commit

---
+++ src/main/java/com/sparta/nbcampspringpersonaltask2/NbcampSpringPersonalTask2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcampspringpersonaltask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampSpringPersonalTask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcampSpringPersonalTask2Application.class, args);
+    }
+
+}
